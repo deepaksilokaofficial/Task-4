e6bc9f3649c546958418ac9c54b3d49063e216a5
deepaksilokaofficial <58412342+deepaksilokaofficial@users.noreply.github.com>
2020-05-26 13:29:29 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -7,8 +7,8 @@ x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-X_train = X_train / 255
-X_test = X_test / 255
+x_train /= 255
+x_test /= 255
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 def baseline_model():
