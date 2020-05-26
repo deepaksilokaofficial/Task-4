1f9fd323a6a30b0393bdef7b8985420f74daf087
deepaksilokaofficial <58412342+deepaksilokaofficial@users.noreply.github.com>
2020-05-26 13:34:25 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -2,24 +2,26 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape(X_train.shape[0], num_pixels)
+X_test = X_test.reshape(X_test.shape[0], num_pixels)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
+num_class=y_test.shape[1]
 def baseline_model():
 	model = Sequential()
-	model.add(Dense(units=512, input_dim=28*28, activation='relu'))
-	model.add(Dense(units=10, activation='softmax'))
+	model.add(Dense(num_pixels, input_dim=num_pixels, activation='relu'))
+	model.add(Dense(num_class, activation='softmax'))
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 model = baseline_model()
 model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=200, verbose=0)
-scores = model.evaluate(x_test, y_test, verbose=0)
+scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy:  %.2f%%" % (scores[1]*100))
 file1 = open("result.txt","w+")
 file1.write(str(scores[1]*100))
