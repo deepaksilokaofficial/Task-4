27af916a089d5f12ca3764ce8558795aec0cd8db
deepaksilokaofficial <58412342+deepaksilokaofficial@users.noreply.github.com>
2020-05-26 13:22:33 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -3,22 +3,22 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
 X_train = X_train / 255
 X_test = X_test / 255
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 def baseline_model():
 	model = Sequential()
-	model.add(Dense(512, input_dim=28*28, activation='relu'))
-	model.add(Dense(10,activation='softmax'))
-	model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['accuracy'])
+	model.add(Dense(units=512, input_dim=28*28, activation='relu'))
+	model.add(Dense(units=10, activation='softmax'))
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 model = baseline_model()
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=1, batch_size=200, verbose=0)
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=200, verbose=0)
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy:  %.2f%%" % (scores[1]*100))
 file1 = open("result.txt","w+")
