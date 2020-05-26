4dbcf31c167ca24baaee5006f61b189da6400f56
deepaksilokaofficial <58412342+deepaksilokaofficial@users.noreply.github.com>
2020-05-26 13:27:25 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -2,7 +2,7 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 x_train = x_train.astype('float32')
@@ -18,8 +18,8 @@ def baseline_model():
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 model = baseline_model()
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=200, verbose=0)
-scores = model.evaluate(X_test, y_test, verbose=0)
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=200, verbose=0)
+scores = model.evaluate(x_test, y_test, verbose=0)
 print("Accuracy:  %.2f%%" % (scores[1]*100))
 file1 = open("result.txt","w+")
 file1.write(str(scores[1]*100))
