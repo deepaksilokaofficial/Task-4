ce965a0fc5c0a5a6b6ed9186f0b2b977c346bb52
deepaksilokaofficial <58412342+deepaksilokaofficial@users.noreply.github.com>
2020-05-26 14:03:40 +0530

    Update MNIST.py

--- MNIST.py
+++ MNIST.py
@@ -4,8 +4,8 @@ from keras.layers import Dense
 from keras.utils import np_utils
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 unit1 = X_train.shape[1] * X_train.shape[2]
-X_train = X_train.reshape(X_train.shape[0], num_pixels)
-X_test = X_test.reshape(X_test.shape[0], num_pixels)
+X_train = X_train.reshape(X_train.shape[0], unit1)
+X_test = X_test.reshape(X_test.shape[0], unit1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
